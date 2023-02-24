a19d0be78960a34f07589d184cad3362f207918c
Douglas Jacobsen <dwjacobsen@google.com>
2023-02-24 10:58:42 -0700

    Update spack runner tests to use config overrides
    This commit updates the remaining spack runner tests to use config
    overrides instead of config.set. This avoids problems where the configs
    clobber user settings.

--- lib/ramble/ramble/test/spack_runner.py
+++ lib/ramble/ramble/test/spack_runner.py
@@ -103,7 +103,7 @@ def test_default_concretize_flags(tmpdir, capsys):
         pytest.skip('%s' % e)
 
 
-def test_config_concretize_flags(tmpdir, capsys, mutable_config):
+def test_config_concretize_flags(tmpdir, capsys):
     try:
         env_path = tmpdir.join('spack-env')
         sr = ramble.spack_runner.SpackRunner(dry_run=True)
@@ -111,11 +111,11 @@ def test_config_concretize_flags(tmpdir, capsys, mutable_config):
         sr.activate()
         sr.add_spec('zlib')
 
-        ramble.config.config.set('config:spack_flags:concretize', '-f --fresh')
-        sr.concretize()
-        captured = capsys.readouterr()
+        with ramble.config.override('config:spack_flags', {'concretize': '-f --fresh'}):
+            sr.concretize()
+            captured = capsys.readouterr()
 
-        assert "with args: ['concretize', '-f', '--fresh']" in captured.out
+            assert "with args: ['concretize', '-f', '--fresh']" in captured.out
     except ramble.spack_runner.RunnerError as e:
         pytest.skip('%s' % e)
 
@@ -143,7 +143,7 @@ def test_default_install_flags(tmpdir, capsys):
         pytest.skip('%s' % e)
 
 
-def test_config_install_flags(tmpdir, capsys, mutable_config):
+def test_config_install_flags(tmpdir, capsys):
     try:
         env_path = tmpdir.join('spack-env')
         sr = ramble.spack_runner.SpackRunner(dry_run=True)
@@ -152,22 +152,22 @@ def test_config_install_flags(tmpdir, capsys, mutable_config):
         sr.add_spec('zlib')
         sr.concretize()
 
-        ramble.config.config.set('config:spack_flags:install', '--fresh --keep-prefix')
-        sr.install()
-        captured = capsys.readouterr()
+        with ramble.config.override('config:spack_flags', {'install': '--fresh --keep-prefix'}):
+            sr.install()
+            captured = capsys.readouterr()
 
-        install_flags = ramble.config.config.get('config:spack_flags:install')
-        expected_str = "with args: ['install'"
-        for flag in install_flags.split():
-            expected_str += f", '{flag}'"
-        expected_str += "]"
+            install_flags = ramble.config.config.get('config:spack_flags:install')
+            expected_str = "with args: ['install'"
+            for flag in install_flags.split():
+                expected_str += f", '{flag}'"
+            expected_str += "]"
 
-        assert expected_str in captured.out
+            assert expected_str in captured.out
     except ramble.spack_runner.RunnerError as e:
         pytest.skip('%s' % e)
 
 
-def test_env_include(tmpdir, capsys, mutable_config):
+def test_env_include(tmpdir, capsys):
     try:
         env_path = tmpdir.join('spack-env')
         sr = ramble.spack_runner.SpackRunner(dry_run=True)
