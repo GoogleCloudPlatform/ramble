32778ea72280a97f62b285deab3c273fa5134c9f
Douglas Jacobsen <dwjacobsen@google.com>
2023-04-13 14:35:09 -0600

    Add test to ensure spack package paths are defined

--- lib/ramble/ramble/test/end_to_end.py
+++ lib/ramble/ramble/test/end_to_end.py
@@ -1039,3 +1039,65 @@ spack:
 
     with open(lock_file, 'r') as f:
         assert 'zlib' in f.read()
+
+
+def test_dryrun_series_contains_package_paths(mutable_config,
+                                              mutable_mock_workspace_path,
+                                              mock_applications):
+    test_config = r"""
+ramble:
+  mpi:
+    command: mpirun
+    args:
+    - '-n'
+    - '{n_ranks}'
+    - '-ppn'
+    - '{processes_per_node}'
+    - '-hostfile'
+    - 'hostfile'
+  batch:
+    submit: 'batch_submit {execute_experiment}'
+  variables:
+    processes_per_node: '10'
+    n_ranks: '{processes_per_node}*{n_nodes}'
+    n_threads: '1'
+  applications:
+    zlib:
+      workloads:
+        ensure_installed:
+          experiments:
+            test_{test_id}:
+              variables:
+                n_nodes: '1'
+                test_id: [1, 2]
+spack:
+  concretized: true
+  compilers: {}
+  mpi_libraries: {}
+  applications:
+    zlib:
+      zlib:
+        base: zlib
+"""
+
+    workspace_name = 'test_dryrun_series_contains_package_paths'
+    ws = ramble.workspace.create(workspace_name)
+    ws.write()
+
+    config_path = os.path.join(ws.config_dir, ramble.workspace.config_file_name)
+
+    with open(config_path, 'w+') as f:
+        f.write(test_config)
+
+    ws.dry_run = True
+    ws._re_read()
+
+    ws.run_pipeline('setup')
+
+    for test in ['test_1', 'test_2']:
+        script = os.path.join(ws.experiment_dir, 'zlib', 'ensure_installed',
+                              test, 'execute_experiment')
+
+        assert os.path.exists(script)
+        with open(script, 'r') as f:
+            assert r'{zlib}' not in f.read()
