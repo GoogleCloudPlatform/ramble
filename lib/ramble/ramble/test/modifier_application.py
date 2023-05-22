64b26c8662a165ba75d7661be9f0bffbbc0d0a48
Douglas Jacobsen <dwjacobsen@google.com>
2023-05-22 15:10:56 -0600

    Add Intel-APS modifier, and tests using it

---
+++ lib/ramble/ramble/test/modifier_application.py
@@ -0,0 +1,90 @@
+# Copyright 2022-2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+# https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+# <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+# option. This file may not be copied, modified, or distributed
+# except according to those terms.
+
+import os
+
+import pytest
+
+import ramble.workspace
+
+from ramble.main import RambleCommand
+
+
+# everything here uses the mock_workspace_path
+pytestmark = pytest.mark.usefixtures('mutable_config',
+                                     'mutable_mock_workspace_path')
+
+workspace = RambleCommand('workspace')
+
+
+def test_wrfv4_aps_test(mutable_config, mutable_mock_workspace_path):
+    test_config = """
+ramble:
+  variables:
+    mpi_command: 'mpirun -n {n_ranks} -ppn {processes_per_node}'
+    batch_submit: 'batch_submit {execute_experiment}'
+    processes_per_node: '10'
+    n_ranks: '{processes_per_node}*{n_nodes}'
+    n_threads: '1'
+  applications:
+    wrfv4:
+      workloads:
+        CONUS_12km:
+          experiments:
+            modifier_test:
+              modifiers:
+              - name: intel-aps
+                mode: mpi
+                on_executable:
+                - '*'
+              variables:
+                n_nodes: '1'
+  spack:
+    concretized: true
+    packages:
+      gcc:
+        spack_spec: gcc@8.5.0
+      intel-mpi:
+        spack_spec: intel-mpi@2018.4.274
+        compiler: gcc
+      wrfv4:
+        spack_spec: wrf@4.2 build_type=dm+sm compile_type=em_real nesting=basic ~chem ~pnetcdf
+        compiler: gcc
+      intel-oneapi-vtune:
+        spack_spec: intel-oneapi-vtune
+    environments:
+      wrfv4:
+        packages:
+        - wrfv4
+        - intel-mpi
+        - intel-oneapi-vtune
+"""
+
+    workspace_name = 'test_wrfv4_modified_aps'
+    with ramble.workspace.create(workspace_name) as ws1:
+        ws1.write()
+
+        config_path = os.path.join(ws1.config_dir, ramble.workspace.config_file_name)
+
+        with open(config_path, 'w+') as f:
+            f.write(test_config)
+
+        ws1._re_read()
+
+        workspace('setup', '--dry-run', global_args=['-w', workspace_name])
+
+        software_path = os.path.join(ws1.software_dir, 'wrfv4.CONUS_12km', 'spack.yaml')
+        with open(software_path, 'r') as f:
+            assert 'intel-oneapi-vtune' in f.read()
+
+        execute_script = os.path.join(ws1.experiment_dir, 'wrfv4', 'CONUS_12km',
+                                      'modifier_test', 'execute_experiment')
+        with open(execute_script, 'r') as f:
+            data = f.read()
+            assert 'aps -c mpi' in data
+            assert 'aps-report' in data
---
+++ var/ramble/repos/builtin/modifiers/intel-aps/modifier.py
@@ -0,0 +1,83 @@
+# Copyright 2022-2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+# https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+# <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+# option. This file may not be copied, modified, or distributed
+# except according to those terms.
+
+from ramble.modkit import *  # noqa: F403
+
+
+class IntelAps(SpackModifier):
+    name = "intel-aps"
+
+    mode('mpi', description='Mode for collecting mpi statistics')
+
+    variable_modification('aps_log_dir', 'aps_{executable_name}_results_dir',
+                          method='set', modes=['mpi'])
+    variable_modification('aps_flags', '-c mpi -r {aps_log_dir}', method='set', modes=['mpi'])
+    variable_modification('mpi_command', 'aps {aps_flags}', method='prepend', modes=['mpi'])
+
+    archive_pattern('aps_*_results_dir/*')
+
+    software_spec('intel-oneapi-vtune', spack_spec='intel-oneapi-vtune')
+
+    required_package('intel-oneapi-vtune')
+
+    executable_modifier('aps_summary')
+
+    def aps_summary(self, executable_name, executable):
+        from ramble.util.executable import CommandExecutable
+
+        pre_exec = []
+        post_exec = []
+        if executable.mpi:
+            post_exec.append(
+                CommandExecutable(f'gen-aps-{executable_name}',
+                                  template=['echo "APS Results for executable {executable_name}"',
+                                            'aps-report -s -D {aps_log_dir}'],
+                                  mpi=False,
+                                  redirect='{log_file}'
+                                  )
+            )
+
+        return pre_exec, post_exec
+
+    figure_of_merit_context('APS Executable',
+                            regex=r'APS Results for executable (?P<exec_name>\w+)',
+                            output_format='APS on {exec_name}')
+
+    summary_foms = [
+        'Application',
+        'Report creation date',
+        'Number of ranks',
+        'Ranks per node',
+        'Used statistics'
+    ]
+
+    for fom in summary_foms:
+        figure_of_merit(fom, fom_regex=r'\s*' + f'{fom}' + r'\s+:\s+(?P<value>.*)', group_name='value',
+                        units='', log_file='{log_file}', contexts=['APS Executable'])
+
+    elapsed_time_regex = r'\s*Elapsed Time:\s*(?P<time>[0-9\.]+)'
+    figure_of_merit('Elapsed Time', fom_regex=elapsed_time_regex, group_name='time',
+                    units='s', log_file='{log_file}', contexts=['APS Executable'])
+
+    mpi_time_regex = r'\s*MPI Time:\s*(?P<time>[0-9\.]+)\s*s\s*(?P<percent>[0-9\.]+)%.*'
+    figure_of_merit('MPI Time', fom_regex=mpi_time_regex, group_name='time',
+                    units='s', log_file='{log_file}', contexts=['APS Executable'])
+    figure_of_merit('MPI Percent', fom_regex=mpi_time_regex, group_name='percent',
+                    units='%', log_file='{log_file}', contexts=['APS Executable'])
+
+    mpi_imba_regex = r'\s*MPI Imbalance:\s*(?P<time>[0-9\.]+)\s*s\s*(?P<percent>[0-9\.]+)%.*'
+    figure_of_merit('MPI Imbalance Time', fom_regex=mpi_imba_regex, group_name='time',
+                    units='s', log_file='{log_file}', contexts=['APS Executable'])
+    figure_of_merit('MPI Imbalance Percent', fom_regex=mpi_imba_regex, group_name='percent',
+                    units='%', log_file='{log_file}', contexts=['APS Executable'])
+
+    disk_io_regex = r'\s*Disk I/O Bound:\s*(?P<time>[0-9\.]+)\s*s\s*(?P<percent>[0-9\.]+)%.*'
+    figure_of_merit('Disk I/O Time', fom_regex=disk_io_regex, group_name='time',
+                    units='s', log_file='{log_file}', contexts=['APS Executable'])
+    figure_of_merit('Disk I/O Percent', fom_regex=disk_io_regex, group_name='percent',
+                    units='%', log_file='{log_file}', contexts=['APS Executable'])
