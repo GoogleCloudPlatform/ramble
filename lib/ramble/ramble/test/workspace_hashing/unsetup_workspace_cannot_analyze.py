95664e32e86b8020ec0d21fec05ec6cd8034b624
Douglas Jacobsen <dwjacobsen@google.com>
2023-09-08 15:49:14 -0600

    Add test to capture non-setup spack envs
    This commit adds a test that exercises analyze on a non-setup workspace
    that uses a spack application.

---
+++ lib/ramble/ramble/test/workspace_hashing/unsetup_workspace_cannot_analyze.py
@@ -0,0 +1,66 @@
+# Copyright 2022-2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+# https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+# <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+# option. This file may not be copied, modified, or distributed
+# except according to those terms.
+
+import os
+
+import pytest
+
+import ramble.application
+import ramble.workspace
+import ramble.config
+import ramble.software_environments
+from ramble.main import RambleCommand, RambleCommandError
+
+
+# everything here uses the mock_workspace_path
+pytestmark = pytest.mark.usefixtures('mutable_config',
+                                     'mutable_mock_workspace_path')
+
+workspace = RambleCommand('workspace')
+
+
+def test_unsetup_workspace_cannot_analyze(mutable_config,
+                                          mutable_mock_workspace_path,
+                                          mock_applications,
+                                          capsys):
+    test_config = """
+ramble:
+  variables:
+    mpi_command: 'mpirun -n {n_ranks} -ppn {processes_per_node}'
+    batch_submit: 'batch_submit {execute_experiment}'
+    partition: 'part1'
+    processes_per_node: '16'
+    n_threads: '1'
+  applications:
+    zlib:
+      workloads:
+        ensure_installed:
+          experiments:
+            simple_test:
+              variables:
+                n_nodes: 1
+              env_vars:
+                set:
+                  MY_VAR: 'TEST'
+  spack:
+    concretized: true
+    packages: {}
+    environments: {}
+"""
+    workspace_name = 'test_unsetup_workspace_cannot_analyze'
+    with ramble.workspace.create(workspace_name) as ws:
+        ws.write()
+
+        config_path = os.path.join(ws.config_dir, ramble.workspace.config_file_name)
+
+        with open(config_path, 'w+') as f:
+            f.write(test_config)
+        ws._re_read()
+        with pytest.raises(RambleCommandError):
+            output = workspace('analyze', global_args=['-w', workspace_name])
+            assert 'spack.lock file does not exist' in output
