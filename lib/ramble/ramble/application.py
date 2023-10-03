66c5c26c8a7b3acbd697150b4844fa36fbdd182f
Bob <robertbird@google.com>
2023-10-03 16:34:38 -0600

    Make license env vars safer

--- lib/ramble/ramble/application.py
+++ lib/ramble/ramble/application.py
@@ -1490,10 +1490,11 @@ class ApplicationBase(object, metaclass=ApplicationMeta):
             license_conf = ramble.config.config.get_config('licenses',
                                                            scope=scope)
             if license_conf:
-                app_licenses = license_conf[self.name]
-                if app_licenses:
-                    # Append logic to source file which contains the exports
-                    command.append(f". {{license_input_dir}}/{self.license_inc_name}")
+                if self.name in license_conf:
+                    app_licenses = license_conf[self.name]
+                    if app_licenses:
+                        # Append logic to source file which contains the exports
+                        command.append(f". {{license_input_dir}}/{self.license_inc_name}")
 
         # Process environment variable actions
         for env_var_set in self._env_variable_sets:
