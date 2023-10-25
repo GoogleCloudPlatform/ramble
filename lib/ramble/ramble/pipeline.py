15a083424c4ca38b0d13a81b373cca8446808328
Bob <robertbird@google.com>
2023-10-25 10:48:54 -0600

    Allow the setup pipeline to skip/fail workspace inventory generation
    Currently any call into setup hard requires an inventory, regardless of
    the phases selected. This causes issues as not all phases are able to
    safely generate the inventory as it depends on the software being
    installed
    
    This PR allows the inventory to be optional and for the execution to
    proceed when non-software phases are selected (eg get_inputs)

--- lib/ramble/ramble/pipeline.py
+++ lib/ramble/ramble/pipeline.py
@@ -319,7 +319,7 @@ class SetupPipeline(Pipeline):
     def __init__(self, workspace, filters):
         super().__init__(workspace, filters)
         self.force_inventory = True
-        self.require_inventory = True
+        self.require_inventory = False
         self.action_string = 'Setting up'
 
     def _prepare(self):
@@ -329,7 +329,12 @@ class SetupPipeline(Pipeline):
         self.workspace.experiments_script = experiment_file
 
     def _complete(self):
-        super()._construct_hash()
+        try:
+            super()._construct_hash()
+        except FileNotFoundError as e:
+            tty.warn("Unable to construct workspace hash due to missing file")
+            tty.warn(e)
+
         self.workspace.experiments_script.close()
         experiment_file_path = os.path.join(self.workspace.root,
                                             self.workspace.all_experiments_path)
