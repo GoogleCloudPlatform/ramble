b1aae35425f629305a3cd7b8dc45030737640481
Douglas Jacobsen <dwjacobsen@google.com>
2024-02-29 12:25:43 -0700

    Add tests to ensure modifier phases work
    This commit adds a test to make sure modifier phases register properly,
    are executed, and are ordered properly as well.

---
+++ var/ramble/repos/builtin.mock/modifiers/mod-phase/modifier.py
@@ -0,0 +1,29 @@
+# Copyright 2022-2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+# https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+# <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+# option. This file may not be copied, modified, or distributed
+# except according to those terms.
+
+from ramble.modkit import *  # noqa: F403
+from ramble.util.logger import logger
+
+
+class ModPhase(BasicModifier):
+    """Define a modifier that defines a new phase with register_phase"""
+    name = "mod-phase"
+
+    tags('test')
+
+    mode('test', description='This is a test mode')
+
+    register_phase('first_phase', pipeline='setup', run_before=['get_inputs'])
+
+    def _first_phase(self, workspace, app_inst=None):
+        logger.all_msg('Inside a phase: first_phase')
+
+    register_phase('after_make_experiments', pipeline='setup', run_after=['make_experiments'])
+
+    def _after_make_experiments(self, workspace, app_inst=None):
+        logger.all_msg('Inside a phase: after_make_experiments')
