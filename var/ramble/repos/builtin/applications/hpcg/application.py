e28c9922101fa89e6811ce60a21e0059daa592a8
domke <673751-domke@users.noreply.gitlab.com>
2024-05-03 18:36:09 +0900

    required package setting breaks benchpark

--- var/ramble/repos/builtin/applications/hpcg/application.py
+++ var/ramble/repos/builtin/applications/hpcg/application.py
@@ -28,8 +28,6 @@ class Hpcg(SpackApplication):
                   spack_spec='hpcg@3.1 +openmp',
                   compiler='gcc9')
 
-    required_package('hpcg')
-
     executable('execute', 'xhpcg', use_mpi=True)
 
     executable('move-log', 'mv HPCG-Benchmark*.txt {out_file}',
--- var/ramble/repos/builtin/applications/osu-micro-benchmarks/application.py
+++ var/ramble/repos/builtin/applications/osu-micro-benchmarks/application.py
@@ -23,8 +23,6 @@ class OsuMicroBenchmarks(SpackApplication):
     software_spec('osu-micro-benchmarks', spack_spec='osu-micro-benchmarks',
                   compiler='gcc')
 
-    required_package('osu-micro-benchmarks')
-
     # Additional variables not yet included: --buffer-num, --validation,
     #  --validation-warmup, --validation-warmup
 
