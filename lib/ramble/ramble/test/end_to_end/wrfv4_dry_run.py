74aa9b0657426275c86659c0a57d98039ec81199
Douglas Jacobsen <dwjacobsen@google.com>
2024-06-05 14:31:04 -0600

    Add a no packager manager version of the wrfv4 dry run test

--- lib/ramble/ramble/test/end_to_end/wrfv4_dry_run.py
+++ lib/ramble/ramble/test/end_to_end/wrfv4_dry_run.py
@@ -24,7 +24,7 @@ pytestmark = pytest.mark.usefixtures("mutable_config", "mutable_mock_workspace_p
 workspace = RambleCommand("workspace")
 
 
-def test_wrfv4_dry_run(mutable_config, mutable_mock_workspace_path):
+def test_wrfv4_spack_dry_run(mutable_config, mutable_mock_workspace_path):
     test_config = """
 ramble:
   variants:
@@ -233,6 +233,229 @@ licenses:
 
                 # Test that spack is used
                 assert "spack env activate" in data
+
+            # Create fake figures of merit.
+            with open(os.path.join(exp_dir, "rsl.out.0000"), "w+") as f:
+                for i in range(1, 6):
+                    f.write(f"Timing for main: time 2019-11-27_00:00:00 on domain 1: {i}{i}.{i}\n")
+                f.write("wrf: SUCCESS COMPLETE WRF\n")
+
+            # Create files that match archive patterns
+            for i in range(0, 5):
+                new_name = "rsl.error.000%s" % i
+                new_file = os.path.join(exp_dir, new_name)
+
+                f = open(new_file, "w+")
+                f.close()
+
+        tmp_results_file = os.path.join(ws1.root, "temp.results.txt")
+        simlink_results_file = os.path.join(ws1.root, "results.latest.txt")
+        # Temporarily store some temp data in the "latest" result and check it
+        # gets updated
+        with open(tmp_results_file, "w+") as f:
+            f.write("Dummy data...")
+        os.symlink(tmp_results_file, simlink_results_file)
+
+        workspace("analyze", "-f", "text", "json", "yaml", global_args=["-w", workspace_name])
+
+        text_results_files = glob.glob(os.path.join(ws1.root, "results*.txt"))
+        json_results_files = glob.glob(os.path.join(ws1.root, "results*.json"))
+        yaml_results_files = glob.glob(os.path.join(ws1.root, "results*.yaml"))
+
+        # Match both the file and the simlink
+        assert len(text_results_files) == 2
+        assert len(json_results_files) == 2
+        assert len(yaml_results_files) == 2
+
+        for text_result in text_results_files:
+            with open(text_result, "r") as f:
+                data = f.read()
+                assert "Tags =" in data
+                assert "Average Timestep Time = 33.3 s" in data
+                assert "Cumulative Timestep Time = 166.5 s" in data
+                assert "Minimum Timestep Time = 11.1 s" in data
+                assert "Maximum Timestep Time = 55.5 s" in data
+                assert "Avg. Max Ratio Time = 0.6" in data
+                assert "Number of timesteps = 5" in data
+
+        workspace("archive", global_args=["-w", workspace_name])
+
+        assert ws1.latest_archive
+        assert os.path.exists(ws1.latest_archive_path)
+
+        for exp in expected_experiments:
+            exp_dir = os.path.join(
+                ws1.latest_archive_path, "experiments", "wrfv4", "CONUS_12km", exp
+            )
+            assert os.path.isdir(exp_dir)
+            assert os.path.exists(os.path.join(exp_dir, "execute_experiment"))
+            assert os.path.exists(os.path.join(exp_dir, "full_command"))
+            assert os.path.exists(os.path.join(exp_dir, "rsl.out.0000"))
+            for i in range(0, 5):
+                assert os.path.exists(os.path.join(exp_dir, f"rsl.error.000{i}"))
+
+
+def test_wrfv4_no_pkg_man_dry_run(mutable_config, mutable_mock_workspace_path):
+    test_config = """
+ramble:
+  variables:
+    mpi_command: 'mpirun -n {n_ranks} -ppn {processes_per_node}'
+    batch_submit: 'batch_submit {execute_experiment}'
+    partition: ['part1', 'part2']
+    processes_per_node: ['16', '36']
+    n_ranks: '{processes_per_node}*{n_nodes}'
+    n_threads: '1'
+  applications:
+    wrfv4:
+      variables:
+        env_name: ['wrfv4', 'wrfv4-portable']
+      workloads:
+        CONUS_12km:
+          experiments:
+            scaling_{n_nodes}_{partition}_{env_name}:
+              success_criteria:
+              - name: 'timing'
+                mode: 'string'
+                match: '.*Timing for main.*'
+                file: '{experiment_run_dir}/rsl.out.0000'
+              env_vars:
+                set:
+                  OMP_NUM_THREADS: '{n_threads}'
+                  TEST_VAR: '1'
+                append:
+                - var-separator: ', '
+                  vars:
+                    TEST_VAR: 'add_var'
+                - paths:
+                    TEST_VAR: 'new_path'
+                prepend:
+                - paths:
+                    TEST_VAR: 'pre_path'
+                unset:
+                - TEST_VAR
+              variables:
+                n_nodes: ['1', '2', '4', '8', '16']
+              matrix:
+              - n_nodes
+              - env_name
+"""
+
+    test_licenses = """
+licenses:
+  wrfv4:
+    set:
+      WRF_LICENSE: port@server
+"""
+
+    workspace_name = "test_end_to_end_wrfv4"
+    with ramble.workspace.create(workspace_name) as ws1:
+        ws1.write()
+
+        config_path = os.path.join(ws1.config_dir, ramble.workspace.config_file_name)
+        license_path = os.path.join(ws1.config_dir, "licenses.yaml")
+
+        aux_software_path = os.path.join(
+            ws1.config_dir, ramble.workspace.auxiliary_software_dir_name
+        )
+        aux_software_files = ["packages.yaml", "my_test.sh"]
+
+        with open(config_path, "w+") as f:
+            f.write(test_config)
+
+        with open(license_path, "w+") as f:
+            f.write(test_licenses)
+
+        for file in aux_software_files:
+            file_path = os.path.join(aux_software_path, file)
+            with open(file_path, "w+") as f:
+                f.write("")
+
+        # Write a command template
+        with open(os.path.join(ws1.config_dir, "full_command.tpl"), "w+") as f:
+            f.write("{command}")
+
+        ws1._re_read()
+
+        workspace("setup", "--dry-run", global_args=["-w", workspace_name])
+
+        out_files = glob.glob(os.path.join(ws1.log_dir, "**", "*.out"), recursive=True)
+
+        assert search_files_for_string(
+            out_files,
+            "Would download https://www2.mmm.ucar.edu/wrf/users/benchmark/v422/v42_bench_conus12km.tar.gz",
+        )  # noqa
+
+        expected_experiments = [
+            "scaling_1_part1_wrfv4",
+            "scaling_2_part1_wrfv4",
+            "scaling_4_part1_wrfv4",
+            "scaling_8_part1_wrfv4",
+            "scaling_16_part1_wrfv4",
+            "scaling_1_part2_wrfv4",
+            "scaling_2_part2_wrfv4",
+            "scaling_4_part2_wrfv4",
+            "scaling_8_part2_wrfv4",
+            "scaling_16_part2_wrfv4",
+            "scaling_1_part1_wrfv4-portable",
+            "scaling_2_part1_wrfv4-portable",
+            "scaling_4_part1_wrfv4-portable",
+            "scaling_8_part1_wrfv4-portable",
+            "scaling_16_part1_wrfv4-portable",
+            "scaling_1_part2_wrfv4-portable",
+            "scaling_2_part2_wrfv4-portable",
+            "scaling_4_part2_wrfv4-portable",
+            "scaling_8_part2_wrfv4-portable",
+            "scaling_16_part2_wrfv4-portable",
+        ]
+
+        # Test experiment directories
+        for exp in expected_experiments:
+            exp_dir = os.path.join(ws1.root, "experiments", "wrfv4", "CONUS_12km", exp)
+            assert os.path.isdir(exp_dir)
+            assert os.path.exists(os.path.join(exp_dir, "execute_experiment"))
+            assert os.path.exists(os.path.join(exp_dir, "full_command"))
+
+            license_inc_path = os.path.join(ws1.root, "shared", "licenses", "wrfv4", "license.inc")
+            with open(os.path.join(exp_dir, "full_command"), "r") as f:
+                data = f.read()
+                # Test the license exists
+                assert f". {license_inc_path}" in data
+
+                # Test the required environment variables exist
+                assert 'export OMP_NUM_THREADS="1"' in data
+                assert "export TEST_VAR=1" in data
+                assert "unset TEST_VAR" in data
+
+                # Test the expected portions of the execution command exist
+                assert "sed -i -e 's/ start_hour.*/ start_hour" in data
+                assert "sed -i -e 's/ restart .*/ restart" in data
+                assert "mpirun" in data
+                assert "wrf.exe" in data
+
+                # Test the run script has a reference to the experiment log file
+                assert os.path.join(exp_dir, f"{exp}.out") in data
+
+            with open(license_inc_path) as f:
+                data = f.read()
+                # Test the license is added to the include file
+                assert "export WRF_LICENSE=port@server" in data
+
+            with open(os.path.join(exp_dir, "execute_experiment"), "r") as f:
+                data = f.read()
+
+                # Test the required environment variables exist
+                assert 'export OMP_NUM_THREADS="1"' in data
+                assert "export TEST_VAR=1" in data
+                assert "unset TEST_VAR" in data
+
+                # Test the expected portions of the execution command exist
+                assert "sed -i -e 's/ start_hour.*/ start_hour" in data
+                assert "sed -i -e 's/ restart .*/ restart" in data
+                assert "mpirun" in data
+                assert "wrf.exe" in data
+
+                # Test the run script has a reference to the experiment log file
+                assert os.path.join(exp_dir, f"{exp}.out") in data
 
             # Create fake figures of merit.
             with open(os.path.join(exp_dir, "rsl.out.0000"), "w+") as f:
