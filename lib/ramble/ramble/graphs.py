853f5ac96f167d7e7e95376d685ede612ccf3992
Bob Bird <robertbird@google.com>
2024-06-13 12:06:35 -0600

    Allow ramble workspace edit to edit licenses.yaml

--- lib/ramble/ramble/graphs.py
+++ lib/ramble/ramble/graphs.py
@@ -107,8 +107,7 @@ class AttributeGraph(object):
                 sorter = graphlib.TopologicalSorter(self.adj_list)
             except AttributeError:
                 logger.die(
-                    "graphlib.TopologicalSorter is not found."
-                    "Ensure requirements.txt are installed (including backports, where needed)."
+                    "graphlib.TopologicalSorter is not found. Ensure requirements.txt are installed (including backports, where needed)."
                 )
 
             try:
