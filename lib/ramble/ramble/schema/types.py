840fe12e04d18990b3f35f403bfc2436b8708f8f
Bob Bird <robertbird@google.com>
2024-07-23 09:27:13 -0600

    Update output capture semantics to not alwyas capture stderr
    This PR makes the semantics around output capture more consistent, moves
    the logic to a single place, and allows ramble to not capture
    stdout/stderr when requested (which currently does not work as expected)

--- lib/ramble/ramble/schema/types.py
+++ lib/ramble/ramble/schema/types.py
@@ -8,12 +8,31 @@
 
 """Base types for building schema files from"""
 
+from enum import Enum
 
-class OUTPUT_CAPTURE:
-    STDERR = "2>"
-    STDOUT = ">>"
-    ALL = "&>"
-    DEFAULT = STDOUT
+
+class OUTPUT_CAPTURE(Enum):
+    STDOUT = 0
+    STDERR = 1
+    ALL = 2
+    DEFAULT = 2
+
+
+class output_mapper:
+    def __init__(self):
+        self.map = {
+            OUTPUT_CAPTURE.STDOUT: ">>",
+            OUTPUT_CAPTURE.STDERR: "2>>",
+            OUTPUT_CAPTURE.ALL: ">>",
+        }
+        self.SUFFIX = "2>&1"
+
+    def generate_out_string(self, out_log, output_operator):
+        redirect_str = f' {self.map.get(output_operator, output_operator)} "{out_log}"'
+        if output_operator is OUTPUT_CAPTURE.ALL:
+            redirect_str += f" {self.SUFFIX}"
+
+        return redirect_str
 
 
 string_or_num_list = [{"type": "string"}, {"type": "number"}]
