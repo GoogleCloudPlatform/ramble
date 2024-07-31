dac0ce604adf853b23d6056a0bf89f62eea9e9a4
David Pomeroy <dpomeroy@google.com>
2024-07-31 04:17:49 +0000

    Add unit tests for Ramble reports

---
+++ lib/ramble/ramble/test/reports.py
@@ -0,0 +1,27 @@
+# Copyright 2022-2024 The Ramble Authors
+#
+# Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+# https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+# <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+# option. This file may not be copied, modified, or distributed
+# except according to those terms.
+
+# Placeholder for reports unit tests
+
+# Test command line args
+
+# Test file import for JSON and YAML
+# - non-repeated experiments
+# - repeated experiments
+
+# Test conversion from nested dict to dataframe
+
+# Test conversion from dataframe to chart-ready pivot
+# - data is not summarized (1 experiment / 1 chart)
+# - data is summarized / not repeats (n experiments / 1 chart)
+# - data is summarized from repeats (n_repeats / 1 chart)
+# - mix of summarized and non-summarized data
+
+# Test that PDF is generated and contains data (size > some value?)
+
+# Possible to test that a specific chart was correctly generated? Not sure...
