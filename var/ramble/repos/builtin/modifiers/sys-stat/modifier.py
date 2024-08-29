b5e365357ce6157528be114a3b6e05df79f16751
Lin Guo <linsword13@gmail.com>
2024-08-29 15:46:47 -0700

    Add a modifier to make it easy to wrap perf tools around an executable
    Given that this allows for custom commands, it does not include any FOMs
    for extracting metrics. It can serve as a base modifier for others to
    extend, such that more well-defined FOMs can be included in the extended
    modifier.
    
    Example usage (run iostat during the app execution):
    
    ```sh
    $ cat $RAMBLE_WORKSPACE/configs/ramble.yaml
    ramble:
      variants:
        package_manager: spack
      variables:
        apply_sampler_exe_regex: 'sleep'
        sampler_cmd: 'iostat -xz 5'
        processes_per_node: 1
        n_nodes: 1
        mpi_command: ''
        batch_submit: '{execute_experiment}'
      applications:
        sleep:
          workloads:
            sleep:
              experiments:
                test1:
                  variables:
                    sleep_seconds: 6
      software:
        packages:
          sysstat:
            pkg_spec: sysstat
        environments:
          hostname:
            packages:
            - sysstat
      modifiers:
      - name: sys-stat
    
    $ ramble on
    
    $ cat $RAMBLE_WORKSPACE/experiments/sleep/sleep/test1/sampler_sleep_linsword-13.c.googlers.com.out
    Linux 6.9.10-1rodete3-amd64 (linsword-13.c.googlers.com)        08/30/2024      _x86_64_        (24 CPU)
    
    avg-cpu:  %user   %nice %system %iowait  %steal   %idle
               0.36    0.51    0.78    0.04    0.06   98.26
    
    Device            r/s     rkB/s   rrqm/s  %rrqm r_await rareq-sz     w/s     wkB/s   wrqm/s  %wrqm w_await wareq-sz     d/s     dkB/s   drqm/s  %drqm d_await dareq-sz     f/s f_await  aqu-sz  %util
    dm-0             5.92    172.10     0.00   0.00    1.09    29.09   26.71    639.42     0.00   0.00   34.52    23.94    0.08    249.38     0.00   0.00    0.50  3024.59    0.00    0.00    0.93   0.63
    dm-1             5.81    171.67     0.00   0.00    1.10    29.53   26.30    638.97     0.00   0.00   13.79    24.30    0.08    249.38     0.00   0.00    0.50  3024.59    0.00    0.00    0.37   0.71
    dm-2             0.10      0.41     0.00   0.00    0.86     4.04    0.11      0.45     0.00   0.00    2.49     4.00    0.00      0.00     0.00   0.00    0.00     0.00    0.00    0.00    0.00   0.00
    sda              5.36    173.66     0.59   9.96    0.99    32.38   19.11    640.35     9.16  32.39    2.69    33.50    0.08    257.40     0.00   0.00    0.52  3110.16    3.22    0.12    0.06   0.55
    
    avg-cpu:  %user   %nice %system %iowait  %steal   %idle
               0.29    0.03    0.56    0.01    0.03   99.07
    
    Device            r/s     rkB/s   rrqm/s  %rrqm r_await rareq-sz     w/s     wkB/s   wrqm/s  %wrqm w_await wareq-sz     d/s     dkB/s   drqm/s  %drqm d_await dareq-sz     f/s f_await  aqu-sz  %util
    dm-0             0.00      0.00     0.00   0.00    0.00     0.00   78.00    304.80     0.00   0.00    1.57     3.91    0.00      0.00     0.00   0.00    0.00     0.00    0.00    0.00    0.12   0.16
    dm-1             0.00      0.00     0.00   0.00    0.00     0.00   78.00    304.80     0.00   0.00    1.57     3.91    0.00      0.00     0.00   0.00    0.00     0.00    0.00    0.00    0.12   0.16
    sda              0.00      0.00     0.00   0.00    0.00     0.00   52.80    304.80    25.20  32.31    1.15     5.77    0.00      0.00     0.00   0.00    0.00     0.00    2.20    0.18    0.06   0.16
    ```

---
+++ var/ramble/repos/builtin/modifiers/sys-stat/modifier.py
@@ -0,0 +1,127 @@
+# Copyright 2022-2024 The Ramble Authors
+#
+# Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+# https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+# <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+# option. This file may not be copied, modified, or distributed
+# except according to those terms
+
+import re
+
+import ramble.util.shell_utils
+from ramble.modkit import *
+from ramble.util.executable import CommandExecutable
+
+
+class SysStat(BasicModifier):
+    """Define a modifier for capturing system metrics.
+
+    Example usage: Below is a ramble config that outputs iostat every 5s
+    for the duration of the sleep application.
+    ```
+    ramble:
+      variants:
+        package_manager: spack
+      variables:
+        apply_sampler_exe_regex: 'sleep'
+        sampler_cmd: 'iostat -xz 5'
+        processes_per_node: 1
+        n_nodes: 1
+        mpi_command: ''
+        batch_submit: '{execute_experiment}'
+    applications:
+      sleep:
+        workloads:
+          sleep:
+            experiments:
+              test1:
+                variables:
+                  sleep_seconds: 6
+    software:
+      packages:
+        sysstat:
+          pkg_spec: sysstat
+      environments:
+        hostname:
+          packages:
+          - sysstat
+    modifiers:
+    - name: sys-stat
+    ```
+    """
+
+    name = "sys-stat"
+
+    tags("system-info", "performance-analysis")
+
+    mode("standard", description="Collect system metrics during program run")
+    default_mode("standard")
+
+    # sysstat contains common tools such as mpstat and iostat
+    software_spec(
+        "sysstat",
+        pkg_spec="sysstat",
+        package_manager="spack*",
+    )
+
+    required_package("sysstat", package_manager="spack*")
+
+    archive_pattern("{experiment_run_dir}/sampler_*")
+
+    modifier_variable(
+        "apply_sampler_exe_regex",
+        default="",
+        description="Apply the sampler when exe_name matches with the regex",
+        mode="standard",
+    )
+
+    modifier_variable(
+        "sampler_cmd",
+        default="mpstat 10",
+        description="Apply the sampler when exec_name matches with the regex",
+        mode="standard",
+    )
+
+    executable_modifier("apply_sampler")
+
+    def apply_sampler(self, exe_name, exe, app_inst=None):
+        pre_cmds = []
+        post_cmds = []
+
+        exe_regex = self.expander.expand_var_name("apply_sampler_exe_regex")
+        applicable = exe_regex and re.match(exe_regex, exe_name)
+
+        if applicable:
+            shell = ramble.config.get("config:shell")
+
+            pre_cmds.append(
+                CommandExecutable(
+                    f"add-sampler-{exe_name}",
+                    template=[
+                        f'log_path="{{experiment_run_dir}}/sampler_{exe_name}_$HOSTNAME.out"',
+                        '{sampler_cmd} > "$log_path" 2>&1 &',
+                        f"sampler_cmd_pid={ramble.util.shell_utils.last_pid_var(shell)}",
+                    ],
+                    mpi=False,
+                    redirect="",
+                    output_capture="",
+                ),
+            )
+
+            post_cmds.append(
+                CommandExecutable(
+                    f"cleanup-sampler-{exe_name}",
+                    template=[
+                        r"""
+if ps -p "$sampler_cmd_pid" > /dev/null; then
+    kill "$sampler_cmd_pid"
+fi
+            """.strip(),
+                    ],
+                    mpi=False,
+                    redirect="",
+                    output_capture="",
+                )
+            )
+
+        return pre_cmds, post_cmds
