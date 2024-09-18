ce85dc22e06c9c154074cf09c7d993a0f64d9fe6
Lin Guo <linsword13@gmail.com>
2024-09-17 17:07:58 -0700

    Add success_criteria

--- var/ramble/repos/builtin/modifiers/tuned-adm/modifier.py
+++ var/ramble/repos/builtin/modifiers/tuned-adm/modifier.py
@@ -11,6 +11,9 @@ import os
 from ramble.modkit import *
 
 
+SUCCESS_STRING = "Status: SUCCESS"
+
+
 class TunedAdm(BasicModifier):
     """Define a modifier for TunedAdm
 
@@ -46,16 +49,14 @@ class TunedAdm(BasicModifier):
         ]
 
     def _prepare_analysis(self, workspace):
-        profile_path = os.path.join(
-            self.expander.expand_var("{experiment_run_dir}"),
-            "tuning_profile",
-        )
+        run_dir = self.expander.expand_var("{experiment_run_dir}")
+        read_profile_path = os.path.join(run_dir, "tuning_profile")
 
-        if not os.path.exists(profile_path):
+        if not os.path.exists(read_profile_path):
             return
 
         profiles = set()
-        with open(profile_path) as f:
+        with open(read_profile_path) as f:
 
             for line in f.readlines():
                 if "active profile:" in line:
@@ -63,14 +64,25 @@ class TunedAdm(BasicModifier):
             profiles_str = ",".join(profiles)
 
         if profiles:
-            with open(profile_path, "a") as f:
+            expected_profile = self.expander.expand_var("{tuned-profile}")
+            write_profile_path = os.path.join(run_dir, "all_tuning_profiles")
+            with open(write_profile_path, "w+") as f:
                 profiles_str = ",".join(profiles)
-                f.write(f"Applied profiles: {profiles_str}")
+                f.write(f"Applied profiles: {profiles_str}\n")
+                if len(profiles) == 1 and profiles_str == expected_profile:
+                    f.write(SUCCESS_STRING)
 
     figure_of_merit(
         "Tuning Profile",
         fom_regex=r"Applied profiles:\s*(?P<profile>.*)",
-        log_file="{experiment_run_dir}/tuning_profile",
+        log_file="{experiment_run_dir}/all_tuning_profiles",
         group_name="profile",
         units="",
+    )
+
+    success_criteria(
+        "Expected tuning profile applied",
+        mode="string",
+        match=SUCCESS_STRING,
+        file="{experiment_run_dir}/all_tuning_profiles",
     )
