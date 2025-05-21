be37668e9756aff55d42feacc5078c4cc9659b6b
Bob Bird <robertbird@google.com>
2025-05-21 14:36:44 -0600

    Add tests for `ramble commands`

--- lib/ramble/ramble/test/commands.py
+++ lib/ramble/ramble/test/commands.py
@@ -6,6 +6,8 @@
 # option. This file may not be copied, modified, or distributed
 # except according to those terms.
 
+import os
+
 import pytest
 
 from ramble.error import RambleCommandError
@@ -27,3 +29,17 @@ def test_available_command():
         logger.msg(f"Command = {command}")
 
         RambleCommand(command)
+
+
+def test_command_output(tmpdir):
+    command = RambleCommand("commands")
+
+    formats = ["subcommands", "rst", "names", "bash"]
+    for f in formats:
+        file = os.path.join(tmpdir, f"outfile.{f}")
+        command("--format", f, "--update", file)
+        assert os.path.isfile(file)
+
+    target = os.path.join(tmpdir, "outfile.names")
+    header = os.path.join(tmpdir, "outfile.subcommands")
+    command("--update", target, "--header", header, "-a")
