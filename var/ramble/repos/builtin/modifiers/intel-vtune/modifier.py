ee63121392ed681fabfe60ca40ae14e5eba049b8
Lin Guo <linsword13@gmail.com>
2025-06-04 16:08:25 -0700

    Add a mod for intel-vtune profiler
    This will only work on VMs with PMU enabled.

---
+++ var/ramble/repos/builtin/modifiers/intel-vtune/modifier.py
@@ -0,0 +1,58 @@
+# Copyright 2022-2025 The Ramble Authors
+#
+# Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+# https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+# <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+# option. This file may not be copied, modified, or distributed
+# except according to those terms.
+
+from ramble.modkit import *
+
+
+class IntelVtune(BasicModifier):
+    """Define a modifier for applying VTune profiling."""
+
+    name = "intel-vtune"
+
+    tags("profiler", "performance-analysis")
+
+    maintainers("linsword13")
+
+    mode("mpi", description="Mode for profiling mpi apps")
+    default_mode("mpi")
+
+    modifier_variable(
+        "vtune_results_dir",
+        default="{experiment_run_dir}/vtune_dir",
+        description="Path to store darshan logs",
+        mode="mpi",
+    )
+
+    modifier_variable(
+        "vtune_args",
+        default="-collect hpc-performance -r {vtune_results_dir}",
+        description="vtune arguments",
+        mode="mpi",
+    )
+
+    archive_pattern("{vtune_results_dir}/*")
+
+    with when("package_manager_family=spack"):
+        software_spec(
+            "vtune",
+            pkg_spec="intel-oneapi-vtune",
+        )
+
+        required_package("intel-oneapi-vtune")
+
+    variable_modification(
+        "mpi_command",
+        "vtune {vtune_args} --",
+        method="append",
+        mode="mpi",
+    )
+
+    register_builtin("setup_vtune_results_dir", injection_method="prepend")
+
+    def setup_vtune_results_dir(self):
+        return ["rm -rf {vtune_results_dir}", "mkdir -p {vtune_results_dir}"]
