c845c4759ec2ee68b30332f9151c8937b6552dde
Lin Guo <linsword13@gmail.com>
2025-11-17 12:45:47 -0800

    Add more type annotations to stats.py

--- lib/ramble/ramble/test/util/stats.py
+++ lib/ramble/ramble/test/util/stats.py
@@ -6,6 +6,8 @@
 # option. This file may not be copied, modified, or distributed
 # except according to those terms.
 
+import math
+
 import pytest
 
 import ramble.util.stats
@@ -120,3 +122,53 @@ import ramble.util.stats
 )
 def test_stats_for_repeat_foms(statistic, input_values, input_units, output):
     assert statistic.report(input_values, input_units) == output
+
+
+def test_calculate_margin_of_error():
+    data = [
+        3.71,
+        4.28,
+        6.93,
+        5.07,
+        5.60,
+        5.45,
+        4.57,
+        5.05,
+        4.78,
+        5.48,
+        4.89,
+        5.65,
+        8.25,
+        6.28,
+        6.38,
+        6.82,
+        2.79,
+        4.65,
+        4.18,
+        4.69,
+        5.90,
+        5.64,
+        4.05,
+        5.33,
+        3.85,
+        4.37,
+        5.03,
+        6.72,
+        4.33,
+        4.77,
+    ]
+
+    assert math.isclose(
+        ramble.util.stats._calculate_margin_of_error(data, 0.99), 0.531, abs_tol=1e-3
+    )
+    assert math.isclose(
+        ramble.util.stats._calculate_margin_of_error(data, 0.95), 0.404, abs_tol=1e-3
+    )
+    assert math.isclose(
+        ramble.util.stats._calculate_margin_of_error(data, 0.90), 0.339, abs_tol=1e-3
+    )
+    assert math.isclose(
+        ramble.util.stats._calculate_margin_of_error(data, 0.50), 0.139, abs_tol=1e-3
+    )
+    with pytest.raises(ValueError, match="Unsupported confidence level"):
+        ramble.util.stats._calculate_margin_of_error(data, 0.1)
