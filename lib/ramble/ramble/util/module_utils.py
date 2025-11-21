6e796fafaf04671b2f75812a47f38f512787d428
Lin Guo <linsword13@gmail.com>
2025-11-20 16:46:46 -0800

    Import pandas dynamically
    The motivation comes from using Python build that supports free-threading. Older versions of pandas don't declare support for free-threading, so importing it statically means all Ramble commands will run with GIL enabled, losing the benefit of free-threading. Changing it to import on-demand, such that commands that don't use this dependency can still run with GIL disabled, if the Python build supports it.
    
    Validation:
    
    ```
    (py314t) linsword-mac:ramble linsword$ ramble -V
    <frozen importlib._bootstrap>:491: RuntimeWarning: The global interpreter lock (GIL) has been enabled to load module 'pandas._libs.pandas_parser', which has not declared that it can run safely without the GIL. To override this behavior and keep the GIL disabled (at your own risk), run with PYTHON_GIL=0 or -Xgil=0.
    0.6.0 (7de39c98863f366b10e74242faa92084d26eeb03)
    (py314t) linsword-mac:ramble linsword$ git stash pop
    (py314t) linsword-mac:ramble linsword$ ramble -V
    0.6.0 (7de39c98863f366b10e74242faa92084d26eeb03)
    ```

---
+++ lib/ramble/ramble/util/module_utils.py
@@ -0,0 +1,35 @@
+# Copyright 2022-2025 The Ramble Authors
+#
+# Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+# https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+# <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+# option. This file may not be copied, modified, or distributed
+# except according to those terms.
+
+"""A utility for dynamically importing modules."""
+
+import importlib
+
+from ramble.util.logger import logger
+
+_modules = {}
+
+
+def import_module(module_name):
+    """A utility for dynamically importing a module."""
+    global _modules  # noqa: F824
+    if module_name not in _modules:
+        try:
+            module = importlib.import_module(module_name)
+            _modules[module_name] = module
+        except ModuleNotFoundError:
+            logger.die(
+                f"`{module_name}` was not found. Ensure all the packages in "
+                "`requirements.txt` are installed."
+            )
+    return _modules[module_name]
+
+
+def import_pandas():
+    """A utility for dynamically importing pandas."""
+    return import_module("pandas")
