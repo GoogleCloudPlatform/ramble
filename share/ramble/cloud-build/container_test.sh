2f9190d32e9590740511dc71af3e00fcc954654f
Douglas Jacobsen <dwjacobsen@google.com>
2026-01-09 09:41:39 -0700

    Add a script to help run unit tests inside of a container
    This commit adds a script to help configure a local container for
    running unit tests.

---
+++ share/ramble/cloud-build/container_test.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Copyright 2022-2026 The Ramble Authors
+#
+# Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+# https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+# <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+# option. This file may not be copied, modified, or distributed
+# except according to those terms.
+
+set -euo pipefail
+
+REPO=${1:-https://github.com/GoogleCloudPlatform/ramble}
+BRANCH=${2:-origin/develop}
+
+rm -rf /workspace
+git clone "$REPO" /workspace
+cd /workspace
+git checkout "$BRANCH"
+
+. /opt/spack/share/spack/setup-env.sh
+spack load py-pip
+
+python -m pip install -r /workspace/requirements-dev.txt
+
+cat > /load_test_env.sh <<'EOF'
+#!/bin/bash
+. /opt/spack/share/spack/setup-env.sh
+spack load py-pip
+export SPACK_PYTHON=$(which python)
+. /workspace/share/ramble/setup-env.sh
+EOF
+
+echo "To load test environment, run:"
+echo ". /load_test_env.sh"
+
