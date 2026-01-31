77f210c1614a3200f3d07237ff89cd027cd0f7e0
Lin Guo <linsword13@gmail.com>
2026-01-30 21:09:02 -0800

    Speed up tests by using a mock spack runner
    The provenance test can take beyond 50s, mostly spending time in spack calls. Now it gets down to 1s on my machine. Similar speedup for the uploader test.

---
+++ lib/ramble/ramble/test/mock_spack_runner.py
@@ -0,0 +1,109 @@
+# Copyright 2022-2026 The Ramble Authors
+#
+# Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+# https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+# <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+# option. This file may not be copied, modified, or distributed
+# except according to those terms.
+
+import os
+
+
+class MockPackageInfo:
+    def __init__(self, data):
+        self.data = data
+
+    def to_dict(self):
+        return self.data
+
+    def to_version_text(self):
+        return f"{self.data.get('name', 'unknown')} @{self.data.get('version', 'unknown')}"
+
+
+class MockSpackRunner:
+    def __init__(self):
+        self.env_path = None
+        self.concretized = False
+        self.dry_run = False
+
+    def set_env(self, env_path, require_exists=True):
+        self.env_path = env_path
+
+    def set_dry_run(self, dry_run):
+        self.dry_run = dry_run
+
+    def set_compiler_config_dir(self, path):
+        pass
+
+    def activate(self):
+        pass
+
+    def deactivate(self):
+        pass
+
+    def concretize(self):
+        if not self.env_path:
+            return
+
+        # write dummy spack.yaml and spack.lock
+        spack_yaml = os.path.join(self.env_path, "spack.yaml")
+        with open(spack_yaml, "w") as f:
+            f.write("specs:\n  - zlib\n")
+
+        spack_lock = os.path.join(self.env_path, "spack.lock")
+        with open(spack_lock, "w") as f:
+            f.write('{"roots": [{"spec":"zlib"}]}')
+
+        self.concretized = True
+
+    def package_provenance(self):
+        return [MockPackageInfo({"name": "zlib", "version": "1.2.11"})]
+
+    def get_version(self):
+        return "0.0.0"
+
+    def generate_source_command(self):
+        return ["echo 'source spack'"]
+
+    def generate_activate_command(self):
+        return ["echo 'activate spack'"]
+
+    def generate_deactivate_command(self):
+        return ["echo 'deactivate spack'"]
+
+    def configure_env(self, env_path):
+        pass
+
+    def inventory_hash(self):
+        return "dummy_hash"
+
+    def package_definitions(self):
+        return []
+
+    def create_env(self, env_path):
+        if not os.path.exists(env_path):
+            os.makedirs(env_path)
+
+    def add_config(self, config):
+        pass
+
+    def add_include_file(self, path):
+        pass
+
+    def copy_from_external_env(self, env):
+        pass
+
+    def add_spec(self, spec):
+        pass
+
+    def generate_env_file(self):
+        pass
+
+    def added_packages(self):
+        return []
+
+    def install(self):
+        pass
+
+    def get_package_path(self, spec):
+        return "zlib", "/path/to/zlib"
