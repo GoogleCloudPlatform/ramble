a709f5bd26c52072ab32a048d39622549f58b43e
Lin Guo <linsword13@gmail.com>
2026-03-27 10:58:02 -0700

    Add in the pr image-build test trigger

---
+++ share/ramble/cloud-build/terraform/triggers/pr_image_build.tf
@@ -0,0 +1,21 @@
+resource "google_cloudbuild_trigger" "pr_image_build_tests" {
+  name        = "PR-Image-Build-Tests"
+  description = "A presubmit check for building images used by other cloud build triggers"
+
+  github {
+    owner = var.github_owner
+    name  = var.github_repo
+    pull_request {
+      branch          = "(?:main|develop)"
+      comment_control = "COMMENTS_ENABLED_FOR_EXTERNAL_CONTRIBUTORS_ONLY"
+    }
+  }
+
+  include_build_logs = "INCLUDE_BUILD_LOGS_WITH_STATUS"
+
+  filename = "share/ramble/cloud-build/ramble-pr-image-builds.yaml"
+
+  included_files = [
+    "share/ramble/cloud-build/**"
+  ]
+}
