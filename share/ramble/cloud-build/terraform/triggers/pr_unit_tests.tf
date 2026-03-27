48dca848247c36e04c4d55f0f10c547a4271c9b1
Lin Guo <linsword13@gmail.com>
2026-03-27 10:39:42 -0700

    Add in the unit-test triggers

---
+++ share/ramble/cloud-build/terraform/triggers/pr_unit_tests.tf
@@ -0,0 +1,35 @@
+resource "google_cloudbuild_trigger" "pr_unit_tests" {
+  for_each = { for img in local.image_matrix : "${img.base}${img.base_ver}-py${img.python}-spack${img.spack}" => img }
+
+  name        = "PR-Unit-Tests-${each.value.base}${replace(each.value.base_ver, ".", "-")}-${replace(each.value.spack, ".", "-")}spack-${replace(each.value.python, ".", "-")}python"
+  description = "Run unit tests and linting on Ramble pull requests"
+
+  github {
+    owner = var.github_owner
+    name  = var.github_repo
+    pull_request {
+      branch          = "(?:main|develop)"
+      comment_control = "COMMENTS_ENABLED_FOR_EXTERNAL_CONTRIBUTORS_ONLY"
+    }
+  }
+
+  ignored_files = [
+    "lib/ramble/docs/**"
+  ]
+
+  included_files = [
+    "lib/ramble/**",
+    "var/ramble/repos/**",
+    "share/ramble/cloud-build/**",
+    "conftest.py"
+  ]
+
+  filename = "share/ramble/cloud-build/ramble-pr-unit-tests.yaml"
+
+  substitutions = {
+    _BASE_IMG   = each.value.base
+    _BASE_VER   = each.value.base_ver
+    _PYTHON_VER = each.value.python
+    _SPACK_REF  = each.value.spack
+  }
+}
