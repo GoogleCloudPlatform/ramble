c325aa38bfce395b1a152530dd4a14734574e6fa
Lin Guo <linsword13@gmail.com>
2026-04-20 16:03:34 -0700

    Add a push trigger to apply terraform configuration
    This ensures changes made to the cloud build triggers are automatically applied upone code merge.

---
+++ share/ramble/cloud-build/terraform/triggers/terraform_apply.tf
@@ -0,0 +1,19 @@
+resource "google_cloudbuild_trigger" "terraform_apply" {
+  name        = "ramble-terraform-apply"
+  description = "Automatically apply Cloud Build Triggers Terraform configuration when pushed to develop branch"
+
+  github {
+    owner = var.github_owner
+    name  = var.github_repo
+    push {
+      branch = "^develop$"
+    }
+  }
+
+  included_files = [
+    "share/ramble/cloud-build/terraform/triggers/**",
+    "share/ramble/cloud-build/ramble-terraform-apply.yaml"
+  ]
+
+  filename = "share/ramble/cloud-build/ramble-terraform-apply.yaml"
+}
