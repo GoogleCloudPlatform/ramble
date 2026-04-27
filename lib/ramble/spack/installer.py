7c4d71f670c04689229f9cd8c141df0c3617f9a2
Lin Guo <linsword13@gmail.com>
2026-04-27 15:17:32 -0700

    Remove unused spack monitor and analyze code
    I encounter this code quality [issue](https://github.com/GoogleCloudPlatform/ramble/security/code-scanning/8) in the embedded spack.monitor code. And it turns out that these part of the code are not used. Plus in the spack codebase the monitor and analyze code are also removed in https://github.com/spack/spack/pull/31130. So removing it to avoid the maintenance burden, plus this helps down-the-line when we decide to upgrade the embedded spack code.

--- lib/ramble/spack/installer.py
+++ lib/ramble/spack/installer.py
@@ -48,7 +48,6 @@ import spack.binary_distribution as binary_distribution
 import spack.compilers
 import spack.error
 import spack.hooks
-import spack.monitor
 import spack.package
 import spack.package_prefs as prefs
 import spack.repo
