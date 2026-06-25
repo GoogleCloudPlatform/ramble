8195e0dd9ef30e64b481de557bea3e19d665e765
Lin Guo <linsword13@gmail.com>
2026-06-25 16:31:05 -0700

    Ensure submodules are exported explicitly
    This detects cases where for instance only `import a` is present, while the code accesses `a.b.func`, where `b` is a submodule that's not automatically imported when only importing module `a`.

--- lib/ramble/ramble/cmd/on.py
+++ lib/ramble/ramble/cmd/on.py
@@ -6,6 +6,7 @@
 # option. This file may not be copied, modified, or distributed
 # except according to those terms.
 
+import ramble.cmd
 import ramble.config
 import ramble.filters
 import ramble.pipeline
