312fa974ce0189991087fd4a315b156cc7a76cdb
Ti Leggett <tileggett@google.com>
2026-07-14 13:27:09 -0500

    Convert to using make_workspace_from_config

--- lib/ramble/ramble/test/success_criteria_functionality/success_precedence.py
+++ lib/ramble/ramble/test/success_criteria_functionality/success_precedence.py
@@ -10,7 +10,6 @@ import os
 
 import pytest
 
-import ramble.workspace
 from ramble.main import RambleCommand
 
 # everything here uses the mock_workspace_path
@@ -20,7 +19,7 @@ workspace = RambleCommand("workspace")
 ramble_on = RambleCommand("on")
 
 
-def test_success_criteria_precedence(mock_applications, workspace_name):
+def test_success_criteria_precedence(mock_applications, make_workspace_from_config):
     """
     Tests that YAML success_criteria takes precedence over
     object-defined criteria
@@ -46,32 +45,23 @@ ramble:
     packages: {}
     environments: {}
 """
-    with ramble.workspace.create(workspace_name) as ws:
-        ws.write()
+    ws, ws_name = make_workspace_from_config(test_config)
 
-        config_path = os.path.join(ws.config_dir, ramble.workspace.CONFIG_FILE_NAME)
+    workspace("setup", global_args=["-w", ws_name])
+    ramble_on(global_args=["-w", ws_name])
+    workspace("analyze", "-f", "text", "json", "yaml", global_args=["-w", ws_name])
 
-        with open(config_path, "w+", encoding="utf-8") as f:
-            f.write(test_config)
-        ws._re_read()
+    for ext in ["txt", "json", "yaml"]:
+        assert os.path.exists(os.path.join(ws.results_dir, f"results.latest.{ext}"))
 
-        workspace("setup", global_args=["-w", workspace_name])
-        ramble_on(global_args=["-w", workspace_name])
-        workspace("analyze", "-f", "text", "json", "yaml", global_args=["-w", workspace_name])
-
-        for ext in ["txt", "json", "yaml"]:
-            assert os.path.exists(os.path.join(ws.results_dir, f"results.latest.{ext}"))
-
-        with open(os.path.join(ws.results_dir, "results.latest.txt"), encoding="utf-8") as f:
-            data = f.read()
-            assert "Success criteria summary:" in data
-            assert (
-                "application::success-criteria-conflicts::_application_function = PASSED" in data
-            )
-            assert "config::experiment::test_success = PASSED" in data
+    with open(os.path.join(ws.results_dir, "results.latest.txt"), encoding="utf-8") as f:
+        data = f.read()
+        assert "Success criteria summary:" in data
+        assert "application::success-criteria-conflicts::_application_function = PASSED" in data
+        assert "config::experiment::test_success = PASSED" in data
 
 
-def test_success_criteria_mutex_versions(mock_applications, workspace_name):
+def test_success_criteria_mutex_versions(mock_applications, make_workspace_from_config):
     """
     Tests that same named success_criteria defined in mutually exclusive
     when clauses are handled appropriately.
@@ -93,32 +83,23 @@ ramble:
     packages: {}
     environments: {}
 """
-    with ramble.workspace.create(workspace_name) as ws:
-        ws.write()
+    ws, ws_name = make_workspace_from_config(test_config)
 
-        config_path = os.path.join(ws.config_dir, ramble.workspace.CONFIG_FILE_NAME)
+    workspace("setup", global_args=["-w", ws_name])
+    ramble_on(global_args=["-w", ws_name])
+    workspace("analyze", "-f", "text", "json", "yaml", global_args=["-w", ws_name])
 
-        with open(config_path, "w+", encoding="utf-8") as f:
-            f.write(test_config)
-        ws._re_read()
+    for ext in ["txt", "json", "yaml"]:
+        assert os.path.exists(os.path.join(ws.results_dir, f"results.latest.{ext}"))
 
-        workspace("setup", global_args=["-w", workspace_name])
-        ramble_on(global_args=["-w", workspace_name])
-        workspace("analyze", "-f", "text", "json", "yaml", global_args=["-w", workspace_name])
-
-        for ext in ["txt", "json", "yaml"]:
-            assert os.path.exists(os.path.join(ws.results_dir, f"results.latest.{ext}"))
-
-        with open(os.path.join(ws.results_dir, "results.latest.txt"), encoding="utf-8") as f:
-            data = f.read()
-            assert "Success criteria summary:" in data
-            assert (
-                "application::success-criteria-conflicts::_application_function = PASSED" in data
-            )
-            assert "application::success-criteria-conflicts::test_version = PASSED" in data
+    with open(os.path.join(ws.results_dir, "results.latest.txt"), encoding="utf-8") as f:
+        data = f.read()
+        assert "Success criteria summary:" in data
+        assert "application::success-criteria-conflicts::_application_function = PASSED" in data
+        assert "application::success-criteria-conflicts::test_version = PASSED" in data
 
 
-def test_success_criteria_multiple_satisfies(mock_applications, workspace_name):
+def test_success_criteria_multiple_satisfies(mock_applications, make_workspace_from_config):
     """
     Tests that an object, Application in this case, defining the same named
     success_criteria in multiple satisfying when conditions, will error out.
@@ -142,27 +123,20 @@ ramble:
     packages: {}
     environments: {}
 """
-    with ramble.workspace.create(workspace_name) as ws:
-        ws.write()
+    ws, ws_name = make_workspace_from_config(test_config)
 
-        config_path = os.path.join(ws.config_dir, ramble.workspace.CONFIG_FILE_NAME)
+    from ramble.error import RambleCommandError
 
-        with open(config_path, "w+", encoding="utf-8") as f:
-            f.write(test_config)
-        ws._re_read()
+    expected_err = (
+        r"Success criteria '.*' in object '.*' is defined "
+        r"multiple times under conflicting satisfied 'when' conditions"
+    )
 
-        from ramble.error import RambleCommandError
-
-        expected_err = (
-            r"Success criteria '.*' in object '.*' is defined "
-            r"multiple times under conflicting satisfied 'when' conditions"
-        )
-
-        with pytest.raises(RambleCommandError, match=expected_err):
-            workspace("setup", global_args=["-w", workspace_name])
+    with pytest.raises(RambleCommandError, match=expected_err):
+        workspace("setup", global_args=["-w", ws_name])
 
 
-def test_success_criteria_inheritance(mock_applications, workspace_name):
+def test_success_criteria_inheritance(mock_applications, make_workspace_from_config):
     """
     Tests that same named success_criteria defined in multiple objects (using
     inheritance) is handled appropriately.
@@ -184,26 +158,17 @@ ramble:
     packages: {}
     environments: {}
 """
-    with ramble.workspace.create(workspace_name) as ws:
-        ws.write()
+    ws, ws_name = make_workspace_from_config(test_config)
 
-        config_path = os.path.join(ws.config_dir, ramble.workspace.CONFIG_FILE_NAME)
+    workspace("setup", global_args=["-w", ws_name])
+    ramble_on(global_args=["-w", ws_name])
+    workspace("analyze", "-f", "text", "json", "yaml", global_args=["-w", ws_name])
 
-        with open(config_path, "w+", encoding="utf-8") as f:
-            f.write(test_config)
-        ws._re_read()
+    for ext in ["txt", "json", "yaml"]:
+        assert os.path.exists(os.path.join(ws.results_dir, f"results.latest.{ext}"))
 
-        workspace("setup", global_args=["-w", workspace_name])
-        ramble_on(global_args=["-w", workspace_name])
-        workspace("analyze", "-f", "text", "json", "yaml", global_args=["-w", workspace_name])
-
-        for ext in ["txt", "json", "yaml"]:
-            assert os.path.exists(os.path.join(ws.results_dir, f"results.latest.{ext}"))
-
-        with open(os.path.join(ws.results_dir, "results.latest.txt"), encoding="utf-8") as f:
-            data = f.read()
-            assert "Success criteria summary:" in data
-            assert (
-                "application::success-criteria-conflicts::_application_function = PASSED" in data
-            )
-            assert "application::success-criteria-conflicts::test_inheritance = PASSED" in data
+    with open(os.path.join(ws.results_dir, "results.latest.txt"), encoding="utf-8") as f:
+        data = f.read()
+        assert "Success criteria summary:" in data
+        assert "application::success-criteria-conflicts::_application_function = PASSED" in data
+        assert "application::success-criteria-conflicts::test_inheritance = PASSED" in data
