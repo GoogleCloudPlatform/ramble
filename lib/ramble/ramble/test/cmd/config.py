9133a34b3e922f90d63a43a1688590536c5cff37
Bob <robertbird@google.com>
2026-07-20 13:39:03 -0600

    Fix test_config_revert to use spack.util.crypto.checksum

--- lib/ramble/ramble/test/cmd/config.py
+++ lib/ramble/ramble/test/cmd/config.py
@@ -7,10 +7,12 @@
 # except according to those terms.
 
 import functools
+import hashlib
 import os
 
 import pytest
 
+import spack.util.crypto
 import llnl.util.filesystem as fs
 
 import ramble.config
@@ -814,11 +816,11 @@ def test_config_revert(config_yaml_v015):
     fs.copy(cfg_file, bkp_file)
 
     config("add", "config:dirty:true")
-    md5cfg = fs.md5sum(cfg_file)
+    md5cfg = spack.util.crypto.checksum(hashlib.md5, cfg_file)
 
     # Check that the backup file exists, compute its md5 sum
     assert os.path.exists(bkp_file)
-    md5bkp = fs.md5sum(bkp_file)
+    md5bkp = spack.util.crypto.checksum(hashlib.md5, bkp_file)
 
     config("revert", "-y", "config")
 
@@ -826,8 +828,9 @@ def test_config_revert(config_yaml_v015):
     # that the md5 sum of the configuration file is the same
     # as that of the old backup file
     assert not os.path.exists(bkp_file)
-    assert md5bkp == fs.md5sum(cfg_file)
+    assert md5bkp == spack.util.crypto.checksum(hashlib.md5, cfg_file)
     assert md5bkp != md5cfg
+
 
 
 # TODO: (dwj) Enable test when we can test properly
