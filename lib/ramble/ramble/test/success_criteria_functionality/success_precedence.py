ca700433002eae81ada89d8aa5b1d4a8c82406cb
Ti Leggett <tileggett@google.com>
2026-07-29 09:35:35 -0500

    Updated tests to use manage experiments instead of explicit YAML

--- lib/ramble/ramble/test/success_criteria_functionality/success_precedence.py
+++ lib/ramble/ramble/test/success_criteria_functionality/success_precedence.py
@@ -10,11 +10,19 @@ import os
 
 import pytest
 
+import ramble.workspace
+from ramble.error import RambleCommandError
 from ramble.main import RambleCommand
 
 # everything here uses the mock_workspace_path
-pytestmark = pytest.mark.usefixtures("mutable_config", "mutable_mock_workspace_path")
+pytestmark = pytest.mark.usefixtures(
+    "mutable_config",
+    "mutable_mock_workspace_path",
+    "mutable_mock_apps_repo",
+    "workspace_deactivate",
+)
 
+config = RambleCommand("config")
 workspace = RambleCommand("workspace")
 ramble_on = RambleCommand("on")
 
@@ -24,6 +32,7 @@ def test_success_criteria_precedence(mock_applications, make_workspace_from_conf
     Tests that YAML success_criteria takes precedence over
     object-defined criteria
     """
+    # TODO: Update once success_criteria has manage experiments command
     test_config = """
 ramble:
   variables:
@@ -61,33 +70,28 @@ ramble:
         assert "config::experiment::test_success = PASSED" in data
 
 
-def test_success_criteria_mutex_versions(mock_applications, make_workspace_from_config):
+def test_success_criteria_mutex_versions(workspace_name):
     """
     Tests that same named success_criteria defined in mutually exclusive
     when clauses are handled appropriately.
     """
-    test_config = """
-ramble:
-  variables:
-    processes_per_node: '1'
-    n_threads: '1'
-  applications:
-    success-criteria-conflicts@1.0.0:
-      workloads:
-        version_wl:
-          experiments:
-            pass-experiment:
-              variables:
-                n_nodes: 1
-  software:
-    packages: {}
-    environments: {}
-"""
-    ws, ws_name = make_workspace_from_config(test_config)
+    global_args = ["-w", workspace_name]
 
-    workspace("setup", global_args=["-w", ws_name])
-    ramble_on(global_args=["-w", ws_name])
-    workspace("analyze", "-f", "text", "json", "yaml", global_args=["-w", ws_name])
+    with ramble.workspace.create(workspace_name) as ws:
+        workspace(
+            "manage",
+            "experiments",
+            "success-criteria-conflicts@1.0.0",
+            "--wf",
+            "version_wl",
+            "--default-variable-value",
+            "1",
+            global_args=global_args,
+        )
+
+    workspace("setup", global_args=global_args)
+    ramble_on(global_args=global_args)
+    workspace("analyze", "-f", "text", "json", "yaml", global_args=global_args)
 
     for ext in ["txt", "json", "yaml"]:
         assert os.path.exists(os.path.join(ws.results_dir, f"results.latest.{ext}"))
@@ -99,33 +103,25 @@ ramble:
         assert "application::success-criteria-conflicts::test_version = PASSED" in data
 
 
-def test_success_criteria_multiple_satisfies(mock_applications, make_workspace_from_config):
+def test_success_criteria_multiple_satisfies(workspace_name):
     """
     Tests that an object, Application in this case, defining the same named
     success_criteria in multiple satisfying when conditions, will error out.
     """
-    test_config = """
-ramble:
-  variables:
-    processes_per_node: '1'
-    n_threads: '1'
-  applications:
-    success-criteria-conflicts:
-      variants:
-        force_pass: True
-      workloads:
-        success_str_wl:
-          experiments:
-            pass-experiment:
-              variables:
-                n_nodes: 1
-  software:
-    packages: {}
-    environments: {}
-"""
-    ws, ws_name = make_workspace_from_config(test_config)
+    global_args = ["-w", workspace_name]
 
-    from ramble.error import RambleCommandError
+    with ramble.workspace.create(workspace_name):
+        workspace(
+            "manage",
+            "experiments",
+            "success-criteria-conflicts",
+            "--wf",
+            "success_str_wl",
+            "--default-variable-value",
+            "1",
+            global_args=global_args,
+        )
+    config("add", "variants:force_pass:true", global_args=global_args)
 
     expected_err = (
         r"Success criteria '.*' in object '.*' is defined "
@@ -133,36 +129,31 @@ ramble:
     )
 
     with pytest.raises(RambleCommandError, match=expected_err):
-        workspace("setup", global_args=["-w", ws_name])
+        workspace("setup", global_args=global_args)
 
 
-def test_success_criteria_inheritance(mock_applications, make_workspace_from_config):
+def test_success_criteria_inheritance(workspace_name):
     """
     Tests that same named success_criteria defined in multiple objects (using
     inheritance) is handled appropriately.
     """
-    test_config = """
-ramble:
-  variables:
-    processes_per_node: '1'
-    n_threads: '1'
-  applications:
-    success-criteria-conflicts:
-      workloads:
-        inheritance_wl:
-          experiments:
-            pass-experiment:
-              variables:
-                n_nodes: 1
-  software:
-    packages: {}
-    environments: {}
-"""
-    ws, ws_name = make_workspace_from_config(test_config)
+    global_args = ["-w", workspace_name]
 
-    workspace("setup", global_args=["-w", ws_name])
-    ramble_on(global_args=["-w", ws_name])
-    workspace("analyze", "-f", "text", "json", "yaml", global_args=["-w", ws_name])
+    with ramble.workspace.create(workspace_name) as ws:
+        workspace(
+            "manage",
+            "experiments",
+            "success-criteria-conflicts",
+            "--wf",
+            "inheritance_wl",
+            "--default-variable-value",
+            "1",
+            global_args=global_args,
+        )
+
+    workspace("setup", global_args=global_args)
+    ramble_on(global_args=global_args)
+    workspace("analyze", "-f", "text", "json", "yaml", global_args=global_args)
 
     for ext in ["txt", "json", "yaml"]:
         assert os.path.exists(os.path.join(ws.results_dir, f"results.latest.{ext}"))
